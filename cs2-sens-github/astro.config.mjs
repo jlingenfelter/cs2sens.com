f089fbdf4f2d5d3bf6641995b6736effd65d35e0
jlingenfelter <145874582+jlingenfelter@users.noreply.github.com>
2026-02-25 12:57:42 +0100

    Update astro.config.mjs

--- cs2-sens-github/astro.config.mjs
+++ cs2-sens-github/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://cs2sens.com',
   integrations: [sitemap()],
   trailingSlash: 'always',
 });
